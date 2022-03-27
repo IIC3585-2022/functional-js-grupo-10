f930b47ff1949a9c580f8e428c9c91871571d51f
mistico0121 <mrschudeck@uc.cl>
2022-03-27 19:41:02 -0300

    multiple players

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const prompt = require("prompt-sync")()
+const _ = require('lodash');
 
 //1) INICIALIZAR EL JUEGO --> array de jugadores (no solo con dos)
-function init_game(name1, name2) {
-  console.log(`Juego inicializado con los jugadores ${name1} y ${name2}\n`)
-  const array_nombre = [name1, name2];
+function init_game(...args) {
+  console.log(`Juego inicializado con los jugadores ${args}\n`)
+  const array_nombre = [...args[0]];
+  console.log("wenaaa")
+  console.log(array_nombre)
   const array_puntaje = array_nombre.map((element) => {
     return [element, 501];
   });
@@ -25,8 +28,8 @@ const ingresar_jugada = (jugador, puntaje, lanzamientos, game) => {
   const bulls = {'DB': 50, 'SB':25}
   const nuevo_puntaje = puntaje - lanzamientos.reduce((prev, curr) => {
     if (bulls[curr]) return prev+bulls[curr]
-    const [mult, puntaje] = curr.split(',').map(elem => +elem)
-    return prev+(mult*puntaje)
+    const [mult, puntaje] = curr.split(',').map(elem => + elem)
+    return prev+(mult * puntaje)
   },0 )
   game[jugador][1] = nuevo_puntaje < 0 ? 0: nuevo_puntaje
 }
@@ -46,20 +49,29 @@ const play_turn = (game, turn, finish) => {
   console.log(`${game[turn][0]} queda con ${game[turn][1]} puntos`)
   finish = check_finish(turn, game)
   console.log(finish ? `Felicidades ${game[turn][0]}`: '')
-  turn = (turn +1)%2
+  console.log("imprimiendo en play turn")
+  console.log(game)
+  console.log(game.length)
+
+  if (turn == (game.length - 1)){
+    turn = 0
+  } else {
+    turn = turn + 1
+  }
+  console.log(`turno ahora es ${turn}`)
   play_turn(game, turn, finish)
 }
-const play_game = (jugador1, jugador2) => {
+const play_game = (...args) => {
   // variables de estado
   let game;
   let turn = 0
   let finish = false;
-  game = init_game(jugador1, jugador2);
+  game = init_game(args);
   turn = 0
   play_turn(game, turn, finish)
 }
 
-play_game('Jaime', 'Ema')
+play_game('Jaime', 'Ema', "Doc")
 
 
 
