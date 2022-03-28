6a399025ebdec1fb8be8c58fb61ada9f2fd511ff
mistico0121 <mrschudeck@uc.cl>
2022-03-27 21:06:55 -0300

    comentarios

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const prompt = require("prompt-sync")()
 
-//1) INICIALIZAR EL JUEGO --> array de jugadores (no solo con dos)
+//INICIALIZAR EL JUEGO --> array de jugadores (usarmos args para que funcione con +2 jugadores)
 function init_game(...args) {
   console.log(`Juego inicializado con los jugadores ${args}\n`)
   const array_nombre = [...args[0]];
+  // Puntajes iniciales
   const array_puntaje = array_nombre.map((element) => {
     return [element, 501];
   });
   return array_puntaje;
 }
 
-//1) OUTPUT [ [ 'Jaime', 150 ], [ 'Emma', 150 ] ]
-
+// Funcion que calcula puntajes despues de ingresar una jugada válida
 const ingresar_jugada = (jugador, puntaje, lanzamientos, game) => {
   const bulls = {'DB': 50, 'SB':25}
   const nuevo_puntaje = puntaje - lanzamientos.reduce((prev, curr) => {
@@ -22,14 +22,17 @@ const ingresar_jugada = (jugador, puntaje, lanzamientos, game) => {
   },0 )
   game[jugador][1] = nuevo_puntaje < 0 ? 0: nuevo_puntaje
 }
+
 const check_finish = (jugador, game) => {
   return game[jugador][1] === 0
 }
 
+// Funcion que formatea el string de jugada ingresado por el usuario
 const format_lanzamientos = (lanzamientos) => {
   return lanzamientos.split(', ').map(elem => elem.replace(/[^a-zA-Z0-9, ]/g, ''));
 }
-// función de diálogo
+
+// Funcion que maneja flujo de turnos
 const play_turn = (game, turn, finish) => {
   if (finish) return;
   const input = prompt(`Ingrese los lanzamientos de ${game[turn][0]}:`)
@@ -48,12 +51,13 @@ const play_turn = (game, turn, finish) => {
 }
 
 const play_game = (...args) => {
-  // variables de estado
+  // Variables de estado del juego
   let game;
   let turn = 0
   let finish = false;
   game = init_game(args);
-  play_turn(game, turn, finish)
+  // Las funciones toman parametros de manera que no afecten parámetros que se encuentran fuera de sus inputs
+  play_turn(game, turn, finish) 
 }
 
 play_game('Jaime', 'Ema', "Doc")
